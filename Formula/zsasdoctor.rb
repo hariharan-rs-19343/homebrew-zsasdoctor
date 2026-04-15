88a75751e7aa78bd5f9ea6c4c1888bb2c60b48f4
hari-19343 <hariharan.rs@zohocorp.com>
2026-04-16 00:35:47 +0530

    fix: use tar.gz instead of zip per brew audit

--- Formula/zsasdoctor.rb
+++ Formula/zsasdoctor.rb
@@ -1,8 +1,8 @@
 class Zsasdoctor < Formula
   desc "SAS Developer Environment Doctor Tool"
   homepage "https://github.com/hariharan-rs-19343/zsasdoctor"
-  url "https://github.com/hariharan-rs-19343/zsasdoctor/archive/refs/tags/v1.0.0.zip"
-  sha256 "653c166ab91ebef257b1d5e518bbf71199a803e5c9ed51a85918596fdb01e4b7"
+  url "https://github.com/hariharan-rs-19343/zsasdoctor/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "1543b5048634cce4ea0f0ed213b9876dc9b60f0dcc980f5a09fa5bbc570e7c43"
   license "MIT"
 
   def install
